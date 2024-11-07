266bde35041058080ff7fcec5be788a4a6d94d79
Njla <nicolasjoselevinalvarez@gmail.com>
2024-11-07 17:52:15 -0300

    Incorporacion basica de metodo de login  - Se implemento un sistema de inicio de sesion, actualmente solo funciona con el archivo admin, cuyas credenciales son:     Usuario: admin     Contraseña: 1  - Se modifico el funcionamiento general para incluir el campo Matricula en Cobranzas.  - La gestion de ID por ahora se mantiene como AUTOINCREMENT (A modificar)

--- control_cobranzas_tkinter.py
+++ control_cobranzas_tkinter.py
@@ -1,17 +1,18 @@
 import sqlite3
 import tkinter as tk
-from tkinter import Button, Entry, Label, StringVar, Toplevel, ttk, simpledialog, messagebox
+from tkinter import END, Button, Entry, Label, StringVar, Toplevel, ttk, simpledialog, messagebox
 from datetime import datetime
+import os
 
 conn = sqlite3.connect('estacionamiento.db')
 cursor = conn.cursor()
 
-def alta_cobranza(monto, moneda, empleado_id):
+def alta_cobranza(matricula,monto, moneda, empleado_id):
     fecha_hora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     cursor.execute("""
     INSERT INTO Cobranzas (Monto, Moneda, Fecha_Hora, Empleado_ID)
-    VALUES (?, ?, ?, ?)
-    """, (monto, moneda, fecha_hora, empleado_id))
+    VALUES (?, ?, ?, ?, ?)
+    """, (matricula, monto, moneda, fecha_hora, empleado_id))
     conn.commit()
     print("Cobranza registrada exitosamente.")
 
@@ -32,7 +33,8 @@ def modificar_cobranza(cobranza_id, nuevo_monto, nueva_moneda, nuevo_empleado_id
     conn.commit()
     print("Cobranza modificada exitosamente.")
 
-#Modifique las consultas de cobranza para que devuelvan los resultados del fetch, para poder trabajarlos mejor con tkinter
+
+#Se modificaron las consultas de cobranza para que devuelvan los resultados del fetch, para poder trabajarlos mejor con tkinter
 
 #Consulta todas las cobranzas existentes
 def consultar_cobranzas():
@@ -49,21 +51,80 @@ def consultar_cobranza_por_id(cobranza_id):
     else:
         print(f"No se encontro ninguna cobranza con ID {cobranza_id}.")
 
+
+def menu_login():
+    def login_verify():
+        username1 = username_verify.get()
+        password1 = password_verify.get()
+        username_entry1.delete(0, END)
+        password_entry1.delete(0, END)
+
+
+        list_of_files = os.listdir()
+        if username1 in list_of_files:
+            file1 = open(username1, "r")
+            verify = file1.read().splitlines()
+            if password1 in verify:
+                ventana_login.destroy()
+                menu_cobranzas()
+            else:
+                messagebox.showwarning("Incorrecto","Usuario o contraseña erroneos, intentelo denuevo.")
+
+        else:
+            messagebox.showwarning("Incorrecto","Usuario o contraseña erroneos, intentelo denuevo.")
+
+
+    
+    ventana_login = tk.Tk()
+    ventana_login.title("Control Cobranzas v1.0")
+    ventana_login.geometry("300x250")
+    Label(ventana_login, text = "Por favor ingrese sus datos para continuar.").pack()
+    Label(ventana_login, text = "").pack()
+
+
+    global username_verify
+    global password_verify
+    
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+
+    global username_entry1
+    global password_entry1
+    
+    Label(ventana_login, text = "Usuario * ").pack()
+    username_entry1 = Entry(ventana_login, textvariable = username_verify)
+    username_entry1.pack()
+    Label(ventana_login, text = "").pack()
+    Label(ventana_login, text = "Contraseña * ").pack()
+    password_entry1 = Entry(ventana_login, textvariable = password_verify)
+    password_entry1.pack()
+    Label(ventana_login, text = "").pack()
+    Button(ventana_login, text = "Iniciar Sesion", width = 10, height = 1, command = login_verify).pack()
+
+    password_entry1.bind('<Return>', lambda event: login_verify())  
+
+    ventana_login.mainloop()
+        
+
 # Menu de cobranzas en Tkinter
 def menu_cobranzas():
 
     # Crea una tabla de la base de datos
     def crear_tabla(ventana):
-        columnas = ("id", "monto", "moneda", "fecha", "empleado")
+        columnas = ("id", "matricula", "monto", "moneda", "fecha", "empleado")
         tabla = ttk.Treeview(ventana, columns=columnas, show="headings")
 
+        
         tabla.heading("id", text="ID")
+        tabla.heading("matricula", text="Matricula")
         tabla.heading("monto", text="Monto")
         tabla.heading("moneda", text="Moneda")
         tabla.heading("fecha", text="Fecha")
         tabla.heading("empleado", text="ID Empleado")
 
         tabla.column("id", width=10)
+        tabla.column("matricula", width=25)
         tabla.column("monto", width=20)
         tabla.column("moneda", width=15)
         tabla.column("fecha", width=50)
@@ -105,7 +166,7 @@ def menu_cobranzas():
     #Alta cobranza
     def agregar_cobranza():
         def confirmar():
-            alta_cobranza(monto.get(), moneda.get(), empleado.get())
+            alta_cobranza(matricula.get(),monto.get(), moneda.get(), empleado.get())
             limpiar_tabla()
             buscar_todos()
             menu_agregar.destroy()
@@ -115,25 +176,29 @@ def menu_cobranzas():
         # ----- Comienzo ------
         menu_agregar = Toplevel(menu)
         menu_agregar.title("Nueva Cobranza")
-        menu_agregar.geometry("300x170")
+        menu_agregar.geometry("300x200")
         menu_agregar.columnconfigure(0, weight=1)
         menu_agregar.columnconfigure(1, weight=1)
         Label(menu_agregar, text = "Ingrese los datos de la nueva cobranza", justify=tk.CENTER).grid(row=0, column=0, columnspan=2, pady=5,padx=5)
         
+        matricula = StringVar()
         monto = StringVar()
         moneda = StringVar()
         empleado = StringVar()
 
-        Label(menu_agregar, text="Monto:").grid(row=1, column=0, sticky=tk.E,pady=5,padx=5)
-        Label(menu_agregar, text="Moneda:").grid(row=2, column=0, sticky=tk.E,pady=5,padx=5)
-        Label(menu_agregar, text="Empleado:").grid(row=3, column=0, sticky=tk.E,pady=5,padx=5)
+        Label(menu_agregar, text="Matricula:").grid(row=1, column=0, sticky=tk.E,pady=5,padx=5)
+        Label(menu_agregar, text="Monto:").grid(row=2, column=0, sticky=tk.E,pady=5,padx=5)
+        Label(menu_agregar, text="Moneda:").grid(row=3, column=0, sticky=tk.E,pady=5,padx=5)
+        Label(menu_agregar, text="Empleado:").grid(row=4, column=0, sticky=tk.E,pady=5,padx=5)
 
+        nueva_matricula = Entry(menu_agregar, textvariable=matricula)
+        nueva_matricula.grid(row=1, column=1,sticky=tk.W)
         nuevo_monto = Entry(menu_agregar, textvariable=monto)
-        nuevo_monto.grid(row=1, column=1,sticky=tk.W)
+        nuevo_monto.grid(row=2, column=1,sticky=tk.W)
         nueva_moneda = Entry(menu_agregar, textvariable=moneda)
-        nueva_moneda.grid(row=2, column=1,sticky=tk.W)
+        nueva_moneda.grid(row=3, column=1,sticky=tk.W)
         nuevo_empleado = Entry(menu_agregar, textvariable=empleado)
-        nuevo_empleado.grid(row=3, column=1,sticky=tk.W)
+        nuevo_empleado.grid(row=4, column=1,sticky=tk.W)
 
         
         cuadro_botones = tk.Frame(menu_agregar)
@@ -172,11 +237,11 @@ def menu_cobranzas():
 
 
             Label(menu_modificar, text="Monto:").grid(row=2, column=0, sticky=tk.E,pady=5,padx=5)
-            Label(menu_modificar, text=f"{cobranza[1]}").grid(row=2, column=1,pady=5,padx=5)
+            Label(menu_modificar, text=f"{cobranza[2]}").grid(row=2, column=1,pady=5,padx=5)
             Label(menu_modificar, text="Moneda:").grid(row=3, column=0, sticky=tk.E,pady=5,padx=5)
-            Label(menu_modificar, text=f"{cobranza[2]}").grid(row=3, column=1,pady=5,padx=5)
+            Label(menu_modificar, text=f"{cobranza[3]}").grid(row=3, column=1,pady=5,padx=5)
             Label(menu_modificar, text="Empleado:").grid(row=4, column=0, sticky=tk.E,pady=5,padx=5)
-            Label(menu_modificar, text=f"{cobranza[4]}").grid(row=4, column=1,pady=5,padx=5)
+            Label(menu_modificar, text=f"{cobranza[5]}").grid(row=4, column=1,pady=5,padx=5)
 
             nuevo_monto = Entry(menu_modificar, textvariable=monto)
             nuevo_monto.grid(row=2, column=2,sticky=tk.W)
@@ -279,5 +344,6 @@ def menu_cobranzas():
     menu.mainloop()
 
 if __name__ == "__main__":
-    menu_cobranzas()
+    menu_login()
+    #menu_cobranzas()
     conn.close()
