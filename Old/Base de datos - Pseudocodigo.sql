63662df919f7b99d65cd2efdd8ff16817a764171
Njla <nicolasjoselevinalvarez@gmail.com>
2024-11-12 21:51:30 -0300

    Creacion de carpeta de archivos antiguos

---
+++ Old/Base de datos - Pseudocodigo.sql
@@ -0,0 +1,35 @@
+TABLE Empleados (
+        DNI INTEGER PRIMARY KEY AUTOINCREMENT,
+        Nombre TEXT NOT NULL,
+        Cargo TEXT NOT NULL,
+        Fecha_Contratacion TEXT NOT NULL
+    )
+    
+TABLE Cobranzas (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Monto REAL NOT NULL,
+        Moneda TEXT NOT NULL,
+        Fecha_Hora TEXT NOT NULL,
+        Empleado_ID INTEGER NOT NULL,
+        FOREIGN KEY (Empleado_ID) REFERENCES Empleados(ID)
+    )
+
+TABLE Usuarios (
+        DNI INTEGER PRIMARY KEY AUTOINCREMENT,
+        Nombre TEXT NOT NULL,
+        Apellido TEXT NOT NULL,
+        Membresia TEXT
+    )
+
+TABLE Estacionamientos (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Fecha_Ingreso TEXT,
+        Fecha_Egreso TEXT,
+        Estado TEXT,
+        Usuario_ID INTEGER NOT NULL,
+        Empleado_ID INTEGER NOT NULL,
+        Cobranza_ID INTEGER,
+        FOREIGN KEY (Usuario_ID) REFERENCES Usuarios(DNI),
+        FOREIGN KEY (Empleado_ID) REFERENCES Empleados(DNI),
+        FOREIGN KEY (Cobranza_ID) REFERENCES Cobranzas(ID)
+    )
